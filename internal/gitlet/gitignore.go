d9d3290198c05a6bee7ff316a6051cb1e9668fcc
Michael Gunkoff <painter@3lines.club>
2023-10-24 12:48:33 +0200

    Update global .gitignore handling in gitignore.go

--- internal/gitlet/gitignore.go
+++ internal/gitlet/gitignore.go
@@ -11,35 +11,35 @@ import (
 const defaultGlobalGitIgnoreFile = ".gitignore_global"
 
 func UpdateGitIgnore() error {
-	// first, check that global .gitignore exists
-	// git config --get core.excludesfile
 	var globalGitIgnoreMustBeUpdated bool
 	globalGitIgnorePath, err := RunCommand("git config --get core.excludesfile", "")
 	fmt.Println("globalGitIgnorePath", globalGitIgnorePath)
 	if err != nil {
-		return fmt.Errorf("failed to get global .gitignore path: %w", err)
-	} else {
-		// check that global .gitignore contains .commit folder
-		var globalGitIgnoreContent []byte
-		globalGitIgnoreContent, err = os.ReadFile(globalGitIgnorePath)
-		if err != nil {
-			return fmt.Errorf("failed to read global .gitignore file: %w", err)
-		}
-
-		fmt.Println("globalGitIgnoreContent:\n", string(globalGitIgnoreContent))
-
-		if !bytes.Contains(globalGitIgnoreContent, []byte(settings.ContextFolder)) {
-			globalGitIgnoreMustBeUpdated = true
-		}
-	}
-
-	// add .commit folder to global .gitignore
-	if globalGitIgnoreMustBeUpdated {
 		homeDir, err := os.UserHomeDir()
 		if err != nil {
 			return fmt.Errorf("failed to get user home directory: %w", err)
 		}
-		globalGitIgnoreFile, err := os.OpenFile(filepath.Join(homeDir, defaultGlobalGitIgnoreFile), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
+		globalGitIgnorePath = filepath.Join(homeDir, defaultGlobalGitIgnoreFile)
+		fmt.Println("⚠️ Unable to read global .gitignore file path, using default one:" + globalGitIgnorePath)
+	}
+
+	fmt.Printf("globalGitIgnorePath: %s, length: %d\n", globalGitIgnorePath, len(globalGitIgnorePath))
+
+	// check that global .gitignore contains .commit folder
+	var globalGitIgnoreContent []byte
+	globalGitIgnoreContent, err = os.ReadFile(globalGitIgnorePath)
+	if err != nil {
+		return fmt.Errorf("failed to read global .gitignore file: %w", err)
+	}
+
+	if !bytes.Contains(globalGitIgnoreContent, []byte(settings.ContextFolder)) {
+		globalGitIgnoreMustBeUpdated = true
+	}
+
+	// add .commit folder to global .gitignore
+	if globalGitIgnoreMustBeUpdated {
+		globalGitIgnoreFile, err := os.OpenFile(globalGitIgnorePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 		if err != nil {
 			return fmt.Errorf("failed to open/create global .gitignore file: %w", err)
 		}
