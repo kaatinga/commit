38435a0846d0c3a643ede935a9ad0df239f5d3f7
Michael Gunkoff <painter@3lines.club>
2023-11-06 11:45:20 +0100

    Update the Push function in internal/gitlet/push.go to use the git.PushOptions{} struct when pushing to remote.

--- internal/gitlet/push.go
+++ internal/gitlet/push.go
@@ -2,12 +2,13 @@ package gitlet
 
 import (
 	"fmt"
+	"github.com/go-git/go-git/v5"
 
 	"github.com/urfave/cli/v2"
 )
 
 func Push(cCtx *cli.Context) error {
-	err := Repo.Push(nil)
+	err := Repo.Push(&git.PushOptions{})
 	if err != nil {
 		return fmt.Errorf("error pushing to remote: %w", err)
 	}
