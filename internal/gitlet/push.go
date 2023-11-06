c1b6c08fa3b9b671e65bc287162cabceea21922a
Michael Gunkoff <painter@3lines.club>
2023-11-06 23:24:04 +0100

    Update the Push function in internal/gitlet/push.go to use the git.PushOptions{} struct and add command output logging.

--- internal/gitlet/push.go
+++ internal/gitlet/push.go
@@ -2,17 +2,14 @@ package gitlet
 
 import (
 	"fmt"
-	"github.com/go-git/go-git/v5"
+	"github.com/kaatinga/commit/internal/settings"
 
 	"github.com/urfave/cli/v2"
 )
 
 func Push(cCtx *cli.Context) error {
-	err := Repo.Push(&git.PushOptions{})
-	if err != nil {
-		return fmt.Errorf("error pushing to remote: %w", err)
-	}
-
-	fmt.Println("🚀 Commit pushed!")
+	stdOut, stdErr := RunCommand("git push", settings.Path)
+	fmt.Println(stdOut)
+	fmt.Println(stdErr)
 	return nil
 }
