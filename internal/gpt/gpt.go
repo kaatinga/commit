0c622ea1c566bb735c4bad396fc164ebe20463b3
Michael Gunkoff <painter@3lines.club>
2023-12-22 13:38:17 +0100

    Update error handling and response processing in OpenAI-related functions.

--- internal/gpt/gpt.go
+++ internal/gpt/gpt.go
@@ -3,6 +3,7 @@ package gpt
 import (
 	"context"
 	"errors"
+	"fmt"
 	"strings"
 	"time"
 
@@ -17,16 +18,10 @@ type OpenAIContextItem struct {
 
 // NewRequest function creates APIContext instance.
 func NewRequest(ctx context.Context, apiKey string, messages []openai.ChatCompletionMessage) (*OpenAIContextItem, error) {
-	client := openai.NewClient(apiKey)
-	resp, err := doOpenAIRequest(ctx, client, messages, 5, openai.GPT3Dot5Turbo1106)
-	if err != nil {
-		return nil, err
-	}
-
-	return newOpenAIResponse(resp.Choices[0].Message)
+	return doOpenAIRequest(ctx, openai.NewClient(apiKey), messages, 5, openai.GPT3Dot5Turbo1106)
 }
 
-func doOpenAIRequest(ctx context.Context, client *openai.Client, messages []openai.ChatCompletionMessage, attempts byte, model string) (openai.ChatCompletionResponse, error) {
+func doOpenAIRequest(ctx context.Context, client *openai.Client, messages []openai.ChatCompletionMessage, attempts byte, model string) (*OpenAIContextItem, error) {
 	resp, err := client.CreateChatCompletion(
 		ctx,
 		openai.ChatCompletionRequest{
@@ -38,7 +33,7 @@ func doOpenAIRequest(ctx context.Context, client *openai.Client, messages []open
 	)
 	if err != nil {
 		if attempts == 0 {
-			return openai.ChatCompletionResponse{}, err
+			return nil, err
 		}
 		var openAIError = new(openai.RequestError)
 		if errors.As(err, &openAIError) {
@@ -49,13 +44,19 @@ func doOpenAIRequest(ctx context.Context, client *openai.Client, messages []open
 				return doOpenAIRequest(ctx, client, messages, attempts-1, model)
 			}
 		}
-		return openai.ChatCompletionResponse{}, err
+		return nil, err
 	}
 
-	return resp, nil
+	output := newOpenAIResponse(resp.Choices[0].Message)
+	if output.Message.Content == "" && attempts > 0 {
+		fmt.Println("it will take more time to generate a commit message, please wait...")
+		return doOpenAIRequest(ctx, client, messages, attempts-1, model)
+	}
+
+	return output, nil
 }
 
-func newOpenAIResponse(response openai.ChatCompletionMessage) (*OpenAIContextItem, error) {
+func newOpenAIResponse(response openai.ChatCompletionMessage) *OpenAIContextItem {
 	gptContext := &OpenAIContextItem{
 		Date:    time.Now().UTC(),
 		Message: response,
@@ -66,11 +67,12 @@ func newOpenAIResponse(response openai.ChatCompletionMessage) (*OpenAIContextIte
 	gptContext.Message.Content, _ = strings.CutPrefix(gptContext.Message.Content, "[openAI]:")
 	gptContext.Message.Content, _ = strings.CutPrefix(gptContext.Message.Content, "Commit message:")
 	gptContext.Message.Content = strings.TrimSpace(gptContext.Message.Content)
+
 	if !split {
 		gptContext.Summary = gptContext.Message.Content
 	} else {
 		gptContext.Summary = strings.TrimSpace(gptContext.Summary)
 	}
 
-	return gptContext, nil
+	return gptContext
 }
